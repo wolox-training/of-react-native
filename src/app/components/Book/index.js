7a640aeae4c3f08bef312e15be9cdd7eb819c03b
Oli <olivia.fernandez@wolox.com.ar>
2020-04-08 16:43:04 -0300

    Add final space.

--- src/app/components/Book/index.js
+++ src/app/components/Book/index.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { string } from 'prop-types';
-import { Text, View, Image, ActivityIndicator } from 'react-native';
+import {string} from 'prop-types';
+import {Text, View, Image, ActivityIndicator} from 'react-native';
 
 import styles from './styles';
 
-function Book({ bookImage, bookTitle, bookAuthor }) {
+function Book({bookImage, bookTitle, bookAuthor}) {
   return (
     <View style={styles.book}>
-      <Image 
-          source={{ uri: bookImage }}
-          style={styles.image}
-          PlaceholderContent={<ActivityIndicator />}
+      <Image
+        source={{uri: bookImage}}
+        style={styles.image}
+        PlaceholderContent={<ActivityIndicator />}
       />
       <View style={styles.bookInfo}>
-          <Text style={styles.title}>{bookTitle}</Text>
-          <Text>{bookAuthor}</Text>
+        <Text style={styles.title}>{bookTitle}</Text>
+        <Text>{bookAuthor}</Text>
       </View>
     </View>
   );
 }
 
 Book.propTypes = {
-    bookImage: string,
-    bookTitle: string.isRequired,
-    bookAuthor: string.isRequired,  
+  bookImage: string,
+  bookTitle: string.isRequired,
+  bookAuthor: string.isRequired,
 };
 
 export default Book;
