89802b3e91ade5cc896d084818fa3df07babb247
Oli <olivia.fernandez@wolox.com.ar>
2020-04-09 11:25:59 -0300

    Refactor based on review.

--- App.js
+++ App.js
@@ -8,9 +8,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Book
-        image={'http://wolox-training.s3.amazonaws.com/uploads/6942334-M.jpg'}
-        title={'Locked in time'}
-        author={'Lois Dunca'}
+        image='http://wolox-training.s3.amazonaws.com/uploads/6942334-M.jpg'
+        title='Locked in time'
+        author='Lois Dunca'
       />
     </View>
   );
