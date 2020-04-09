5c36462227af1949d167cd54ab6ac50c0c64c070
Oli <olivia.fernandez@wolox.com.ar>
2020-04-09 12:09:50 -0300

    Refactor library component into hooks.

--- src/app/components/Library/index.js
+++ src/app/components/Library/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
 import { arrayOf } from 'prop-types';
 import { FlatList, SafeAreaView } from 'react-native';
 
@@ -7,21 +7,25 @@ import { bookProps } from '@models/propTypes';
 
 import styles from './styles';
 
-class Library extends Component {
-  renderBook = ({ item }) => (
-    <Book bookTitle={item.title} bookAuthor={item.author} bookImage={item.image_url} />
+function Library({ books }) {
+  const renderBook = useCallback(
+    ({ item }) => (
+      <Book title={item.title} author={item.author} image={item.image_url} />
+    ),
+    []
   );
 
-  extractBookId = item => item.id.toString();
+  const extractBookId = useCallback(item => item.id.toString(), []);
 
-  render() {
-    const { books } = this.props;
-    return (
-      <SafeAreaView style={styles.library}>
-        <FlatList data={books} renderItem={this.renderBook} keyExtractor={this.extractBookId} />
-      </SafeAreaView>
-    );
-  }
+  return (
+    <SafeAreaView style={styles.library}>
+      <FlatList
+        data={books}
+        renderItem={renderBook}
+        keyExtractor={extractBookId}
+      />
+    </SafeAreaView>
+  );
 }
 
 Library.propTypes = {
