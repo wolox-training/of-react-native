9e463bfbb72a6ae3c3c64ce1dae48f9820c3d9b0
Oli <olivia.fernandez@wolox.com.ar>
2020-04-09 10:15:15 -0300

    Add space between brackets in Book styles.

--- src/app/components/Book/styles.js
+++ src/app/components/Book/styles.js
@@ -1,5 +1,5 @@
-import {StyleSheet} from 'react-native';
-import {COLORS} from './../../../constants/colors';
+import { StyleSheet } from 'react-native';
+import { COLORS } from '@constants/colors';
 
 export default StyleSheet.create({
   book: {
