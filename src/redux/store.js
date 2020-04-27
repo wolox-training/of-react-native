2b747294349aaeab702b7629742aed632cdd67cb
Oli <olivia.fernandez@wolox.com.ar>
2020-04-27 12:31:42 -0300

    Remove devtool.

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,4 @@ import thunk from 'redux-thunk';
 
 import login from './auth/reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default createStore(login, composeEnhancers(applyMiddleware(thunk)));
+export default createStore(login, compose(applyMiddleware(thunk)));
