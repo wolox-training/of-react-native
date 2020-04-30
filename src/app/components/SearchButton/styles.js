7a527aa7dcec3e7f60e7f517852fcec8331aace2
Oli <olivia.fernandez@wolox.com.ar>
2020-04-30 14:20:14 -0300

    Adjust search icon in header.

--- src/app/components/SearchButton/styles.js
+++ src/app/components/SearchButton/styles.js
@@ -3,7 +3,9 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   image: {
     marginRight: 15,
-    marginTop: 5
+    marginTop: 5,
+    width: 30,
+    height: 30
   }
 });
 
