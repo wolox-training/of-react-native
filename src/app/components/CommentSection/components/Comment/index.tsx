ce504ab451130908f71c89c061a013d3d0f9ae04
Oli <olivia.fernandez@wolox.com.ar>
2020-05-14 09:20:33 -0300

    Pass comment section to typescript.

--- src/app/components/CommentSection/components/Comment/index.tsx
+++ src/app/components/CommentSection/components/Comment/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { string } from 'prop-types';
 import { Text, View, Image } from 'react-native';
 
 import styles from './styles';
 
-function Comment({ image, text, user }) {
+interface CommentProps {
+  image: string;
+  text: string;
+  user: string;
+}
+
+function Comment({ image, text, user } : CommentProps) {
   return (
     <View style={styles.container}>
       <Image source={{ uri: image }} style={styles.image} />
@@ -20,12 +25,6 @@ Comment.defaultProps = {
   image: 'https://i.ya-webdesign.com/images/default-avatar-png-18.png',
   text: '',
   user: ''
-};
-
-Comment.propTypes = {
-  image: string.isRequired,
-  text: string.isRequired,
-  user: string.isRequired
 };
 
 export default Comment;
