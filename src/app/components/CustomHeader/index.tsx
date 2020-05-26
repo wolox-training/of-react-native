98db6ce1f55d013fca0c9fa08938cf6d53364b3f
Oli <olivia.fernandez@wolox.com.ar>
2020-05-26 14:39:42 -0300

    Change import.

--- src/app/components/CustomHeader/index.tsx
+++ src/app/components/CustomHeader/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { Header } from '@react-navigation/stack';
-import { StackHeaderProps } from '@react-navigation/stack/lib/typescript/src/types';
+import { StackHeaderProps } from '@react-navigation/stack';
 
 import { COLORS } from '@constants/colors';
 
