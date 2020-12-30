05ac1446f929cae67cccac452bb17b5c6372da0a
Mariano Zicavo <marianozicavo@users.noreply.github.com>
2020-12-30 16:06:16 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import store from '@redux/store';
+import store from  '@redux/store';
 
 import AppWithNavigation from './src/app';
 
